0f4f59ab9de075a90c6821314068d661ac70f75a
dp <codewithdp2073@gmail.com>
2026-08-02 07:47:04 +0530

    fix: remove duplicate case-tracked Navbar folder, keep lowercase navbar

--- frontend/src/shared/components/layout/Navbar/common/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as NavbarLogo } from "./NavbarLogo";
-export { default as NotificationButton } from "./NotificationButton";
-export { default as SearchInput } from "./SearchInput";
-export { default as ThemeToggle } from "./ThemeToggle";
-export { default as MobileMenuButton } from "./MobileMenuButton";
